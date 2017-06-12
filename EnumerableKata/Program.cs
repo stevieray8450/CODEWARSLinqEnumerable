69b2be0c9cc3d2b413b8a6f2b9ac3884fb9d3b08
Stephen Boniface <stevieray8450@gmail.com>
2017-06-11 22:13:25 -0500

    first commit

---
+++ EnumerableKata/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace EnumerableKata
+{
+    class KataFun
+    {
+        public static int KataInt(int value)
+        {
+            return Enumerable.Range(0, value)
+                             .Where(x => x % 3 == 0 || x % 5 == 0)
+                             .Sum();
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            KataFun.KataInt(10);
+        }
+    }
+}
